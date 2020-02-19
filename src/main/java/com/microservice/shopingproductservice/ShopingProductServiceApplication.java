ee6d7762434093e1a0679bd26bbaa2d1828bd11f
emmy <munezemmy17@gmail.com>
2020-02-19 11:14:08 -0600

    v1

---
+++ src/main/java/com/microservice/shopingproductservice/ShopingProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.shopingproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopingProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopingProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/shopingproductservice/ShopingProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.shopingproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopingProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
